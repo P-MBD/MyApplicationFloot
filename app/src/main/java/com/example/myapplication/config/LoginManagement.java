a15acb5ac9815f43787400790d614da7263d3402
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-07-31 11:38:22 +0330

    Session3-part

---
+++ app/src/main/java/com/example/myapplication/config/LoginManagement.java
@@ -0,0 +1,32 @@
+package com.example.myapplication.config;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.myapplication.models.User;
+
+public class LoginManagement {
+    Context context;
+    SharedPreferences sh;
+    SharedPreferences.Editor editor;
+
+    public LoginManagement(Context context){
+        this.context = context;
+        sh = context.getSharedPreferences("loginData", Context.MODE_PRIVATE);
+        editor = sh.edit();
+    }
+
+    public  void saveData(User user){
+        editor.putString("userName", user.getUserName());
+        editor.putString("password", user.getPassword());
+        editor.commit();
+    }
+    public String getUserName(){
+        return  sh.getString("username","");
+    }
+
+    public  String getPassword(){
+        return  sh.getString("password","");
+    }
+
+}
