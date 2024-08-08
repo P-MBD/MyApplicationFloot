df4144eba396f62ad4b1212adb20816f34854916
P-MBD <p.mobedi@gmail.com>
2024-08-08 07:06:34 +0330

    Session4-part1-ViewBinding

--- app/src/main/java/com/example/myapplication/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/myapplication/adapter/ProductAdapter.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.myapplication.R;
 
@@ -37,20 +36,17 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder_Mohammadi onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.product_row, parent, false);
-                return new ViewHolder_Mohammadi(v);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ProductRowBinding binding = ProductRowBinding.inflate(inflater, parent, false);
+        return new ViewHolder_Mohammadi(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder_Mohammadi holder, int position) {
-
-      Product product = productList.get(position);
-      holder.txt_title.setText(product.getTitle());
-      Picasso.with(context).load(product.getIcon()).error(R.mipmap.ic_launcher).into(holder.img_app);
-//        //holder.binding.imgApp
-//        holder.binding.txtTitle.setText(product.getTitle());
-//        Picasso.with(context).load(product.getIcon()).error(R.mipmap.ic_launcher).into(holder.binding.imgApp);
+        Product product = productList.get(position);
+        //holder.binding.imgApp
+        holder.binding.txtTitle.setText(product.getTitle());
+        Picasso.with(context).load(product.getIcon()).error(R.mipmap.ic_launcher).into(holder.binding.imgApp);
     }
 
     @Override
@@ -59,14 +55,12 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     }
 
     public class ViewHolder_Mohammadi extends RecyclerView.ViewHolder {
-       // private final ProductRowBinding binding;
-       public TextView txt_title;
-       public  ImageView img_app;
-        public ViewHolder_Mohammadi(View v) {
-            super(v);
-            //this.binding = binding;
-            txt_title = v.findViewById(R.id.txt_title);
-            img_app = v.findViewById(R.id.img_app);
+        private final ProductRowBinding binding;
+
+        //  ImageView img_app;
+        public ViewHolder_Mohammadi(ProductRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
 
 
